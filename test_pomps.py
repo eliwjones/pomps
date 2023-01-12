e3eb7cb2c467d7e40cc7359aea5392d9591b251f
Mr. Eli W. Jones <eli.jones@gmail.com>
2023-01-12 05:59:29 +0000

    test_pomps.py: load_and_transform_source_data() - with grouping.  Up line length to 139.

--- test_pomps.py
+++ test_pomps.py
@@ -70,7 +70,56 @@ class TestPomps(unittest.TestCase):
                 json.dumps({'id': 0, 'name': 'Robert Smith', 'classification': 'p*mp'}),
                 json.dumps({'id': 2, 'name': 'Joe J', 'classification': 'player'}),
                 json.dumps({'id': 1, 'name': 'Bill B', 'classification': 'witness'}),
-                ''
+                '',
+            ]
+        )
+
+        self.assertEqual(Path(transformed_path).read_text(), expected)
+
+    def test_load_and_transform_source_data_with_grouping(self):
+        name = 'some_name_for_data'
+
+        def transform_func(data):
+            new_data = {'id': None, 'names': [], 'classifications': []}
+            for d in data['data']:
+                new_data['id'] = d['_id']
+                new_data['names'].append(d['name'].title())
+                new_data['classifications'].append(d['type'].replace('pimp', 'p*mp'))
+
+            return new_data
+
+        def load_func(filepath):
+            test_jsonl = [
+                {'_id': 0, 'name': 'bob smith', 'type': 'player'},
+                {'_id': 0, 'name': 'robert smith', 'type': 'pimp'},
+                {'_id': 2, 'name': 'joe j', 'type': 'player'},
+                {'_id': 1, 'name': 'bill b', 'type': 'witness'},
+                {'_id': 0, 'name': 'rsmith', 'type': 'witness'},
+            ]
+
+            Path(filepath).parent.mkdir(parents=True, exist_ok=True)
+            Path(filepath).write_text('\n'.join(map(json.dumps, test_jsonl)))
+
+        def group_key_func(data):
+            return str(data['_id'])
+
+        transformed_path = pomps.load_and_transform_source_data(
+            name,
+            transform_func,
+            load_func,
+            env='testing',
+            execution_date=datetime.now(),
+            root_dir=TEST_DATA,
+            group_key_func=group_key_func,
+            group_buckets=2,
+        )
+
+        expected = '\n'.join(
+            [
+                json.dumps({'id': 0, 'names': ['Bob Smith', 'Robert Smith', 'Rsmith'], 'classifications': ['player', 'p*mp', 'witness']}),
+                json.dumps({'id': 2, 'names': ['Joe J'], 'classifications': ['player']}),
+                json.dumps({'id': 1, 'names': ['Bill B'], 'classifications': ['witness']}),
+                '',
             ]
         )
 
